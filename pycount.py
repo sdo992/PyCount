94af9b4cedb924728a3f71bcdf4d9af7fe384977
Steven Osborne <46057937+sdo992@users.noreply.github.com>
2023-12-12 12:11:54 -0900

    Update pycount.py
    Now multiprocessor capable; completely replaced code with updated code

--- pycount.py
+++ pycount.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import multiprocessing
 import time
 import sys
 import os
 from datetime import timedelta
 
+def count_up(start, end, result_queue):
+    count = 0
+    for i in range(start, end):
+        count += 1
+    result_queue.put(count)
+
 def uin_Int():
-    os.system('clear') #Clear the screen before beginning
-    uin = input("Enter a number, or, 'q' to quit: ")
+    os.system('clear') # Let's start clean
+    uin = input("Enter a number, or, 'q' to quit: ") # Accepting as a string, will change to integer later
     if uin == 'q':
         os.system('clear') #Why not?
         print("Goodbye!")
@@ -14,31 +21,43 @@ def uin_Int():
         os.system('clear') #Keeping it clean
         sys.exit(0) #Exit the program
     try:
-        nuin = int(uin)
+        nuin = int(uin) #Make sure it's an integer
         return nuin #Make this available
     except ValueError:
         print("I need an integer to continue.")
         time.sleep(1.5) #Arbitrary time to keep error message on screen
         return(uin_Int())
+
+def main():
+    nuin = int(uin_Int()) # Convert string into integer
+    print("    Counting to " + format(nuin, ',d')) #Format output to a more readable number
+    num_processors = os.cpu_count() # Get the number of cores/threads
+    print("    Number of cores to be used: " + str(num_processors))
     
-nuin = uin_Int()
-print("Counting to " + format(nuin, ',d')) #Format output to a more readable number
-a = 1 # Declare variable before time stamp and loop
-start = time.time() # Get the current time
+    chunk_size = nuin // num_processors # Break up the user number as equally as possible
+    start = 0
+    
+    result_queue = multiprocessing.Queue()
+    processes = []
+    
+    start_time = time.time()
+    
+    for _ in range(num_processors):
+        end = start + chunk_size
+        process = multiprocessing.Process(target=count_up, args=(start, end, result_queue))
+        processes.append(process)
+        start = end
+    
+    for process in processes:
+        process.start()
+    
+    for process in processes:
+        process.join()
+        
+    total_count = sum(result_queue.get() for _ in range(num_processors))
+    end_time = time.time()
+    endtd = timedelta(seconds=(end_time - start_time)) # How long did it take?
+    print("    Completed LOOP in: " + str(endtd)[:-3] + " seconds") #Limit to three decimal points and keep it clean
 
-while a <= nuin:
-    a = a + 1
-ending = time.time() # Get the time at which the while loop ended
-stp = str(round(ending - start, 3)) #Limit to three decimal places and convert to a string
-endtd = timedelta(seconds=(ending - start)) #Evaluating a different method to show time
-print("Completed WHILE LOOP in: " + stp + " seconds")
-print("Completed WHILE LOOP in: " + str(endtd)[:-3] + " seconds") #Limit to three decimal points
-
-fstart = time.time() #Evaluating difference between WHILE and FOR loops
-for i in range(nuin):
-	a += 1
-fending = time.time()
-fstp = str(round(fending - fstart, 3))
-fendtd = timedelta(seconds=(fending - fstart))
-print("Completed FOR LOOP in: " + fstp + " seconds")
-print("Completed FOR LOOP in: " + str(fendtd)[:-3] + " seconds")
+if __name__ == "__main__":
+    main()
