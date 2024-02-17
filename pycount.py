59a30fc0733a94e103c5a85a6d6583105c44e450
Steven Osborne <46057937+sdo992@users.noreply.github.com>
2024-02-17 14:00:46 -0900

    Update pycount.py
    Added user-selected input for number of CPU cores to use; changed uin_Int() function to be more efficient

--- pycount.py
+++ pycount.py
@@ -12,29 +12,51 @@ def count_up(start, end, result_queue):
     result_queue.put(count)
 
 def uin_Int():
-    os.system('clear') # Let's start clean
-    uin = input("Enter a number, or, 'q' to quit: ") # Accepting as a string, will change to integer later
-    if uin == 'q':
-        os.system('clear') #Why not?
-        print("Goodbye!")
-        time.sleep(2) #Arbitrary time to keep the goodbye on screen
-        os.system('clear') #Keeping it clean
-        sys.exit(0) #Exit the program
-    try:
-        nuin = int(uin) #Make sure it's an integer
-        return nuin #Make this available
-    except ValueError:
-        print("I need an integer to continue.")
-        time.sleep(1.5) #Arbitrary time to keep error message on screen
-        return(uin_Int())
+    while True:
+        os.system('clear')
+        uin = input("Enter a number or 'q' to quit: ")
+        try:
+            if uin == 'q':
+                os.system('clear')
+                print("Goodbye!")
+                time.sleep(2.0)
+                os.system('clear')
+                sys.exit(0)
+            else:
+                nuin = int(uin)
+                break
+        except ValueError:
+            os.system('clear')
+            print("Error! You need to enter an integer.")
+            time.sleep(2.0)
+    return nuin
+
+def ucpu():
+    while True:
+        num_processors = os.cpu_count()
+        print("Number of CPU cores available: " + str(num_processors))
+        ucpui = input("Enter the number of cores you would like to use: ")
+        try:
+            ucpue = int(ucpui)
+            if ucpue <= num_processors:
+                break
+            else:
+                os.system('clear')
+                print("Error! The number you entered exceeds the amount of CPU cores available. Try again.")
+                time.sleep(2.0)
+        except ValueError:
+            os.system('clear')
+            print("Error! The value entered must be an integer.")
+            time.sleep(2.0)
+    return ucpue
 
 def main():
-    nuin = int(uin_Int()) # Convert string into integer
+    nuin = int(uin_Int())
+    ucpue = int(ucpu())
     print("    Counting to " + format(nuin, ',d')) #Format output to a more readable number
-    num_processors = os.cpu_count() # Get the number of cores/threads
-    print("    Number of cores to be used: " + str(num_processors))
+    print("    Using total number of CPU cores: " + str(ucpue))
     
-    chunk_size = nuin // num_processors # Break up the user number as equally as possible
+    chunk_size = nuin // ucpue
     start = 0
     
     result_queue = multiprocessing.Queue()
@@ -42,7 +64,7 @@ def main():
     
     start_time = time.time()
     
-    for _ in range(num_processors):
+    for _ in range(ucpue):
         end = start + chunk_size
         process = multiprocessing.Process(target=count_up, args=(start, end, result_queue))
         processes.append(process)
@@ -54,10 +76,10 @@ def main():
     for process in processes:
         process.join()
         
-    total_count = sum(result_queue.get() for _ in range(num_processors))
+    total_count = sum(result_queue.get() for _ in range(ucpue))
     end_time = time.time()
     endtd = timedelta(seconds=(end_time - start_time)) # How long did it take?
-    print("    Completed LOOP in: " + str(endtd)[:-3] + " seconds") #Limit to three decimal points and keep it clean
+    print("    Completed in: " + str(endtd)[:-3] + " seconds") #Limit to three decimal points and keep it clean
 
 if __name__ == "__main__":
     main()
